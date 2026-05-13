c1f5b56234d8cc1083cbd6e5cd8842a1ce257a62
Miguel Lopes <migueldollopes@gmail.com>
2026-05-13 16:45:20 -0300

    CRUD completo na API de vegetais

--- 13-05/src/routes/vegetableRoutes.js
+++ 13-05/src/routes/vegetableRoutes.js
@@ -5,7 +5,7 @@ const route = express.Router();
 route.get('/', async (req, res) => {
     const vegetables = await vegetablesServices.getAllVegetables();
     res.json(vegetables);
-})
+});
 
 route.get('/:id', async (req, res) => {
     const vegetable = await vegetablesServices.getById(req.params.id)
@@ -13,8 +13,46 @@ route.get('/:id', async (req, res) => {
     if (!vegetable) return res.status(404).json({
         message: 'Vegetable not found'
     })
-
     res.json(vegetable)
+});
+
+route.post('/', async (req, res) => {
+    const { name, price, amount } = req.body
+    const vegetable = await vegetablesServices.createVegetable(name, price, amount);
+
+    res.status(201).json(vegetable)
+});
+
+route.put('/:id', async (req, res) => {
+    const { name, price, amount } = req.body
+    const vegetable = await vegetablesServices.updateVegetable(req.params.id, name, price, amount);
+
+    if (!vegetable) return res.status(404).json({
+        message: 'Vegetable not found'
+    })
+    res.json(vegetable);
+});
+
+route.patch('/:id', async (req, res) => {
+    const { name, price, amount } = req.body
+    const vegetable = await vegetablesServices.patchVegetable(req.params.id, name, price, amount);
+
+    if (!vegetable) return res.status(404).json({
+        message: 'Vegetable not found'
+    })
+    res.json(vegetable)
+});
+
+route.delete('/:id', async (req, res) => {
+    const vegetable = await vegetablesServices.deleteVegetables(req.params.id)
+
+    if (!vegetable) return res.status(404).json({
+        message: 'Vegetable not found'
+    })
+    res.status(200).json({
+        message: 'Vegetable deleted',
+        deleted: vegetable
+    })
 })
 
 export default route;
--- 13-05/src/services/vegetableServices.js
+++ 13-05/src/services/vegetableServices.js
@@ -15,6 +15,38 @@ class VegetablesServices {
         );
         return result.rows[0];
     }
+
+    async createVegetable(name, price, amount) {
+        const result = await pool.query(
+            'INSERT INTO public."vegetables" (name, price, amount) VALUES ($1, $2, $3) RETURNING *',
+            [name, price, amount]
+        );
+        return result.rows[0];
+    }
+
+    async updateVegetable(id, name, price, amount) {
+        const result = await pool.query(
+            'UPDATE public."vegetables" SET name = $1, price = $2, amount = $3 WHERE id = $4 RETURNING *',
+            [name, price, amount, id]
+        );
+        return result.rows[0];
+    }
+
+    async patchVegetable(id, name, price, amount) {
+        const result = await pool.query(
+            'UPDATE public."vegetables" SET name = COALESCE($1, name), price = COALESCE($2, price), amount = COALESCE($3, amount) WHERE id = $4 RETURNING *',
+            [name, price, amount, id]
+        )
+        return result.rows[0];
+    }
+
+    async deleteVegetables(id) {
+        const result = await pool.query(
+            'DELETE FROM public."vegetables" WHERE id = $1 RETURNING *',
+            [id]
+        )
+        return result.rows[0];
+    }
 };
 
 export const vegetablesServices = new VegetablesServices();
